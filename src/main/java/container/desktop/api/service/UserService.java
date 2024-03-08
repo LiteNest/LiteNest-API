9b1c28f2447d0d05514643102429ee7322cccd69
等陌上花开 <zhzhn2002@gmail.com>
2024-03-08 21:51:25 +0800

    修正登录过程中的一些问题

--- src/main/java/container/desktop/api/service/UserService.java
+++ src/main/java/container/desktop/api/service/UserService.java
@@ -1,6 +1,9 @@
 package container.desktop.api.service;
 
 import container.desktop.api.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -12,13 +15,18 @@ public interface UserService<U extends User> extends EntityService<U>{
     Status register(String username, String password);
     Status changePassword(String username, String password);
     String encryptPassword(String rawPassword);
+    U getByUsername(String username);
 
+    @Getter
+    @AllArgsConstructor
     enum Status {
-        SUCCESS,
-        USER_ALREADY_EXISTS,
-        USER_NOT_FOUND,
-        INVALID_SECRET,
-        FAILED;
+        SUCCESS("成功"),
+        USER_ALREADY_EXISTS("用户已存在"),
+        USER_NOT_FOUND("用户不存在"),
+        INVALID_SECRET("凭据无效"),
+        FAILED("");
+        @Setter
+        private String message;
 
     }
 }
